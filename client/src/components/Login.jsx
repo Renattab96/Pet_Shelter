5a8e1a2965944247fd40c322221a3aed6432ec32
David Fernandez <carmeloferso@hotmail.com>
2023-03-15 11:26:34 -0300

    arreglo de codigo

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,10 +5,8 @@ import '../style/login.css'
 
 
 const Login = () => {
-
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-
     const navigate = useNavigate()
 
     const submitHandler = (e) => {
@@ -17,34 +15,26 @@ const Login = () => {
             email, password
         }, { withCredentials: true })
             .then((res) => {
-
                 navigate('/todospet')
                 // console.log(res)
-
             }).catch((err) => {
                 console.log(err)
             })
     }
 
-
-
     return (
         <div className='bg-image'>
-
             <header>
                 <h2 className='logo' >Pet Shelter</h2>
             </header>
             <div className='form-container'>
                 <form onSubmit={submitHandler} className="formclass" action="#" >
-
                     <div className="userDiv">
                         {/* <label htmlFor="" className='flabel'>Email:</label> */}
                         <input type="text" className='form-control' onChange={(e) => setEmail(e.target.value)} placeholder='Introduzca su email' />
                     </div>
                     <div className='passDiv'>
-
                         <input type="password" className='form-control' onChange={(e) => setPassword(e.target.value)} placeholder='Introduzca su Contrasena' />
-
                     </div>
                     <div className='btnDiv' >
                         <button>Login</button>
@@ -55,7 +45,6 @@ const Login = () => {
                 </form>
             </div>
         </div>
-
     )
 }
 
